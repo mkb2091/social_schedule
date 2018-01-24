f65e27f6ac6b392257f70348eedcbc07f5055ff3
Hugh Williams <hughaw64@gmail.com>
2018-01-24 20:24:47 +0000

    Made code use flat array instead of multi-dimensional vector

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ use std::collections::HashSet;
 extern crate rand;
 use rand::{thread_rng, Rng};
 
-fn get_score(event: Vec<Vec<Vec<u8>>>) -> f32 {
+fn get_score(event: [u8; 144]) -> f32 {
     let mut game: Vec<HashSet<u8>> = Vec::with_capacity(24);
     let mut opponents: Vec<HashSet<u8>> = Vec::with_capacity(24);
     for _ in 0..24 {
         game.push(HashSet::with_capacity(6));
         opponents.push(HashSet::with_capacity(19));
     }
-    for r in event {
+    for r in 0..6 {
+        let r: usize = r * 24;
         let mut i: u8 = 0;
-        for table in r {
+        for t in 0..6 {
+            let t: usize = t * 4;
             i += 1;
-            for player in table.iter() {
-                let player: usize = *player as usize;
+            for player in 0..4 {
+                let player: usize = event[r + t + player] as usize;
                 game[player].insert(i);
-                for p in table.iter() {
-                    opponents[player].insert(*p);
+                for p in 0..4 {
+                    let p: u8 = event[r + t + p];
+                    opponents[player].insert(p);
 
                 }
             }
@@ -25,7 +28,7 @@ fn get_score(event: Vec<Vec<Vec<u8>>>) -> f32 {
     }
     let mut score: f32 = 0.0;
     for i in game {
-        score += i.len() as f32
+        score += i.len() as f32;
     }
     let mut min: f32 = 18.0;
     let mut total: f32 = 0.0;
@@ -41,17 +44,12 @@ fn get_score(event: Vec<Vec<Vec<u8>>>) -> f32 {
     score
 }
 
-fn gen_layout() -> Vec<Vec<Vec<u8>>> {
+fn gen_layout() -> [u8; 144] {
     let mut rng = thread_rng();
-    let mut event: Vec<Vec<Vec<u8>>> = Vec::with_capacity(6);
-    event.push(vec![
-        vec![0, 1, 2, 3],
-        vec![4, 5, 6, 7],
-        vec![8, 9, 10, 11],
-        vec![12, 13, 14, 15],
-        vec![16, 17, 18, 19],
-        vec![20, 21, 22, 23],
-    ]);
+    let mut event: [u8; 144] = [0; 144];
+    for i in 0..24 {
+        event[i as usize] = i;
+    }
     let mut options = vec![
         0,
         1,
@@ -78,59 +76,39 @@ fn gen_layout() -> Vec<Vec<Vec<u8>>> {
         22,
         23,
     ];
-    for _ in 1..6 {
-        let mut round: Vec<Vec<u8>> = Vec::with_capacity(6);
+    for r in 1..6 {
         rng.shuffle(&mut options);
-        for t in 0..6 {
-            let mut table: Vec<u8> = Vec::with_capacity(4);
-            for p in 0..4 {
-                table.push(options[4 * t + p])
-            }
-            round.push(table);
+        for pos in 0..24 {
+            event[r * 24 + pos as usize] = options[pos as usize];
         }
-        event.push(round);
     }
     event
 }
 
-//fn mutate(mut event: Vec<Vec<Vec<u8>>>) -> Vec<Vec<Vec<u8>>> {
-//    let mut rng = thread_rng();
-//    let r: usize = rng.gen_range(0, 6);
-//    let t1: usize = rng.gen_range(0, 6);
-//    let mut t2: usize = rng.gen_range(0, 6);
-//    while t1 == t2 {
-//        t2 = rng.gen_range(0, 6);
-//    }
-//    let p1: usize = rng.gen_range(0, 4);
-//    let p2: usize = rng.gen_range(0, 4);
-//    let person1: u8 = event[r][t1][p1];
-//    event[r][t1][p1] = event[r][t1][p2];
-//    event[r][t2][p2] = person1;
-//    event.to_vec()
-//}
-
 fn main() {
     println!("Welcome to Social Scheduler");
-    let mut event: Vec<Vec<Vec<u8>>> = gen_layout();
-    let mut cevent: Vec<Vec<Vec<u8>>>;
+    let mut event: [u8; 144] = gen_layout();
+    let mut cevent: [u8; 144];
     let mut score: f32 = get_score(event.clone());
     let mut max: f32 = score;
     let mut iterations: u32 = 0;
-    let mut calculations: u32 = 0;
     loop {
         iterations += 1;
         let mut changed: bool = false;
-        let mut new: Vec<Vec<Vec<u8>>> = Vec::with_capacity(6);
+        let mut new: [u8; 144] = [0; 144];
         for r in 0..6 {
+            let r: usize = r * 24;
             for t1 in 0..6 {
+                let t1: usize = t1 * 4;
                 for t2 in 0..6 {
+                    let t2: usize = t2 * 4;
                     if t1 != t2 {
                         for p1 in 0..4 {
                             for p2 in 0..4 {
                                 cevent = event.clone();
-                                let person1: u8 = cevent[r][t1][p1];
-                                cevent[r][t1][p1] = cevent[r][t1][p2];
-                                cevent[r][t2][p2] = person1;
+                                let person1: u8 = cevent[r + t1 + p1];
+                                cevent[r + t1 + p1] = cevent[r + t2 + p2];
+                                cevent[r + t2 + p2] = person1;
                                 let cscore: f32 = get_score(cevent.clone());
                                 if cscore > score {
                                     score = cscore;
@@ -143,7 +121,6 @@ fn main() {
                 }
             }
         }
-        calculations += 6 * 6 * 5 * 4 * 4;
         if changed {
             event = new;
         } else {
@@ -152,13 +129,24 @@ fn main() {
                     "Found local max: {:?} after {:?} iterations ({:?} attempts)",
                     score,
                     iterations,
-                    calculations
+                    6 * 6 * 5 * 4 * 4 * iterations,
                 );
                 max = score;
-                println!("{:?}", event);
+                let mut json_event: Vec<Vec<Vec<u8>>> = Vec::with_capacity(6);
+                for r in 0..6 {
+                    let mut round: Vec<Vec<u8>> = Vec::with_capacity(6);
+                    for t in 0..6 {
+                        let mut table: Vec<u8> = Vec::with_capacity(4);
+                        for p in 0..4 {
+                            table.push(event[r * 24 + t * 4 + p]);
+                        }
+                        round.push(table);
+                    }
+                    json_event.push(round);
+                }
+                println!("{:?}", json_event);
             }
             iterations = 0;
-            calculations = 0;
             event = gen_layout();
             score = get_score(event.clone());
         }
