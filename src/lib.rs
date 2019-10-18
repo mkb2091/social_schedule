2edec2c54f01280cf54e445e29286d87f42b0d75
Alex Williams <hughaw64@gmail.com>
2019-10-18 21:22:12 +0100

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,30 @@ use seed::prelude::*;
 
 #[derive(Clone)]
 enum Page {
-	GenerateSchedule,
-	ManagePlayers,
-	ManageGroups
+    GenerateSchedule,
+    ManagePlayers,
+    ManageGroups,
 }
 
 struct Model {
     pub val: i32,
-    pub page: Page
+    pub page: Page,
 }
 
 impl Default for Model {
     fn default() -> Self {
         Self {
             val: 0,
-            page: Page::GenerateSchedule
+            page: Page::GenerateSchedule,
         }
     }
 }
-
 
 // Update
 
 #[derive(Clone)]
 enum Msg {
-	ChangePage(Page),
+    ChangePage(Page),
     Increment,
 }
 
@@ -40,35 +39,39 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     }
 }
 
-
 // View
 
 fn view(model: &Model) -> impl View<Msg> {
-vec![
-title![format!("{}", match model.page {
-    Page::GenerateSchedule => "Generate Schedule",
-    Page::ManagePlayers => "Manage Players",
-    Page::ManageGroups => "Manage Groups",
-    })],
-    h1![format!("{}", match model.page {
-    Page::GenerateSchedule => "Generate Schedule",
-    Page::ManagePlayers => "Manage Players",
-    Page::ManageGroups => "Manage Groups",
-    })],
-    button![
-        simple_ev(Ev::Click, Msg::ChangePage(Page::GenerateSchedule)),
-        "Generate Schedule"
-    ],
-    button![
-        simple_ev(Ev::Click, Msg::ChangePage(Page::ManagePlayers)),
-        "Manage Players"
-    ],
-    button![
-        simple_ev(Ev::Click, Msg::ChangePage(Page::ManageGroups)),
-        "Manage Groups"
-    ],]
-    
-    
+    vec![
+        title![format!(
+            "{}",
+            match model.page {
+                Page::GenerateSchedule => "Generate Schedule",
+                Page::ManagePlayers => "Manage Players",
+                Page::ManageGroups => "Manage Groups",
+            }
+        )],
+        h1![format!(
+            "{}",
+            match model.page {
+                Page::GenerateSchedule => "Generate Schedule",
+                Page::ManagePlayers => "Manage Players",
+                Page::ManageGroups => "Manage Groups",
+            }
+        )],
+        button![
+            simple_ev(Ev::Click, Msg::ChangePage(Page::GenerateSchedule)),
+            "Generate Schedule"
+        ],
+        button![
+            simple_ev(Ev::Click, Msg::ChangePage(Page::ManagePlayers)),
+            "Manage Players"
+        ],
+        button![
+            simple_ev(Ev::Click, Msg::ChangePage(Page::ManageGroups)),
+            "Manage Groups"
+        ],
+    ]
 }
 
 #[wasm_bindgen(start)]
@@ -76,5 +79,4 @@ pub fn render() {
     seed::App::build(|_, _| Model::default(), update, view)
         .finish()
         .run();
-
 }
