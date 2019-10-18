0a7efe7b14a01ee19acbbc93775dc8b87b14c059
Alex Williams <hughaw64@gmail.com>
2019-10-18 22:35:56 +0100

    Added player list which can be added to

--- src/lib.rs
+++ src/lib.rs
@@ -10,55 +10,80 @@ enum Page {
     ManageGroups,
 }
 
+struct GenerateSchedule {
+    pub players: Vec<String>,
+    pub add_player_input: String,
+}
+
 struct Model {
-    pub val: i32,
     pub page: Page,
+    pub generate_schedule: GenerateSchedule,
 }
 
 impl Default for Model {
     fn default() -> Self {
         Self {
-            val: 0,
             page: Page::GenerateSchedule,
+            generate_schedule: GenerateSchedule {
+                players: vec![],
+                add_player_input: String::new(),
+            },
         }
     }
 }
 
-// Update
-
 #[derive(Clone)]
 enum Msg {
     ChangePage(Page),
-    Increment,
+    GSAddPlayer,
+    GSAddPlayerInput(String),
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => model.val += 1,
         Msg::ChangePage(page) => model.page = page,
+        Msg::GSAddPlayerInput(player) => {
+            model.generate_schedule.add_player_input = player;
+        }
+        Msg::GSAddPlayer => {
+            let player = &model.generate_schedule.add_player_input;
+            if !player.is_empty() {
+                model.generate_schedule.players.push(player.clone());
+                model.generate_schedule.add_player_input = String::new();
+            }
+        }
     }
 }
 
-// View
+fn view_generate_schedule(model: &Model) -> Node<Msg> {
+    div![div![
+        h2!["Player list"],
+        ul![{
+            let mut players_list: Vec<Node<Msg>> =
+                Vec::with_capacity(model.generate_schedule.players.len());
+            for player in &model.generate_schedule.players {
+                players_list.push(li![player, button!["Remove"]]);
+            }
+            players_list
+        }],
+        span!["Player Name: "],
+        input![input_ev(Ev::Input, Msg::GSAddPlayerInput)],
+        button![simple_ev(Ev::Click, Msg::GSAddPlayer), "Add"],
+    ]]
+}
 
 fn view(model: &Model) -> impl View<Msg> {
     vec![
-        title![format!(
-            "{}",
-            match model.page {
-                Page::GenerateSchedule => "Generate Schedule",
-                Page::ManagePlayers => "Manage Players",
-                Page::ManageGroups => "Manage Groups",
-            }
-        )],
-        h1![format!(
-            "{}",
-            match model.page {
-                Page::GenerateSchedule => "Generate Schedule",
-                Page::ManagePlayers => "Manage Players",
-                Page::ManageGroups => "Manage Groups",
-            }
-        )],
+        title![match model.page {
+            Page::GenerateSchedule => "Generate Schedule",
+            Page::ManagePlayers => "Manage Players",
+            Page::ManageGroups => "Manage Groups",
+        }],
+        h1![match model.page {
+            Page::GenerateSchedule => "Generate Schedule",
+            Page::ManagePlayers => "Manage Players",
+            Page::ManageGroups => "Manage Groups",
+        }],
         button![
             simple_ev(Ev::Click, Msg::ChangePage(Page::GenerateSchedule)),
             "Generate Schedule"
@@ -71,6 +96,10 @@ fn view(model: &Model) -> impl View<Msg> {
             simple_ev(Ev::Click, Msg::ChangePage(Page::ManageGroups)),
             "Manage Groups"
         ],
+        match model.page {
+            Page::GenerateSchedule => view_generate_schedule(model),
+            _ => div![],
+        },
     ]
 }
 
