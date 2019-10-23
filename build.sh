23e7d9b88115ce44ed1f3b0116bbb4d6cbe90a68
Alex Williams <hughaw64@gmail.com>
2019-10-23 23:04:42 +0100

    Now build script generates single html page with all resources included

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-cargo make build_release && wasm-opt -O4 pkg/package_bg.wasm -o pkg/package_bg.wasm
+set -e
+cargo make build_release
+wasm-opt -O4 pkg/package_bg.wasm -o pkg/package_bg.wasm
+python3 generate_html.py
---
+++ generate_html.py
@@ -0,0 +1,12 @@
+import base64
+with open('src/index.html') as file:
+    data = file.read()
+
+with open('pkg/package.js') as file:
+    js = file.read()
+
+with open('pkg/package_bg.wasm', 'rb') as file:
+    wasm = file.read()
+
+with open('index.html', 'w') as file:
+    file.write(data.format(js=js, wasm=list(wasm)))
