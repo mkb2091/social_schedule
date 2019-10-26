ab53aafca421c0b76d710e63b0d357c378a7dd44
Alex Williams <hughaw64@gmail.com>
2019-10-26 13:14:33 +0100

    Changed generation of index.html

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ cargo make build_release
 echo "Optimising WASM"
 wasm-opt -O4 pkg/package_bg.wasm -o pkg/package_bg.wasm
 echo "Optimising JavaScript"
-terser pkg/package.js -c -m > pkg/package_temp.js 
-mv pkg/package_temp.js pkg/package.js 
+sed -i '/export default init;/d' pkg/package.js
+python3 -c "with open('pkg/package_bg.wasm', 'rb') as file:print('init(Uint8Array.from(%s));' % list(file.read()))" >> pkg/package.js
+terser pkg/package.js -c -m > pkg/package_temp.js
+mv pkg/package_temp.js pkg/package.js
 echo "Generating index.html"
-python3 generate_html.py
+python3 -c "print(open('src/index.html').read().format(js=open('pkg/package.js').read()))" > index.html
