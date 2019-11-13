e8bb976bdfed031a33fe5b20e5f285c17f19a539
Alex Williams <mkb2191@protonmail.com>
2019-11-13 09:49:55 +0000

    Added AddPlayerEmailInput Event

--- src/lib.rs
+++ src/lib.rs
@@ -81,6 +81,7 @@ pub enum Msg {
     GSGenerate,
     MPAddPlayer,
     MPAddPlayerNameInput(String),
+    MPAddPlayerEmailInput(String),
     MPRemovePlayer(u32),
     MGAddGroup,
     MGAddGroupNameInput(String),
@@ -110,6 +111,9 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
         Msg::MPAddPlayerNameInput(player_name) => {
             model.manage_players.set_player_name_input(player_name)
         }
+        Msg::MPAddPlayerEmailInput(player_email) => {
+	    model.manage_players.set_player_email_input(player_email)
+	    }
         Msg::MPAddPlayer => model.manage_players.add_player(&mut model.database),
         Msg::MPRemovePlayer(id) => model.manage_players.remove_player(&mut model.database, id),
         Msg::MGAddGroup => model.manage_groups.add_group(&mut model.database),
