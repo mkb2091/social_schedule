3812c5522583b9c96c68fbc5a3201e452c11cdf0
Alex Williams <hughaw64@gmail.com>
2020-06-05 10:28:25 +0100

    Moved cfg

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn display_schedule(
 ) {
     let mut output = String::new();
     output.push_str("Testing ");
-    let per_second = (operations as f64 / (nanos as f64 / 10_f64.powi(9))) as u64;
+    let per_second = (10_f64.powi(9) * operations as f64 / nanos as f64) as u64;
     output.write_formatted(&per_second, &Locale::en).unwrap();
     output.push_str(" schedules per second\n");
     output.push_str("Total schedules tested:");
@@ -107,66 +107,66 @@ fn display_schedule(
     println!("{}", output);
 }
 
+#[cfg(feature = "cli")]
 fn main() {
-    #[cfg(feature = "cli")]
+    let opts: Opts = Opts::parse();
+    let (tx, rx) = std::sync::mpsc::channel::<schedule::Schedule>();
+    let operations = std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0));
+    let random_starts = std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0));
+    println!("Spawning {} threads", num_cpus::get());
+    for _ in 0..num_cpus::get() {
+        let tx = tx.clone();
+        let operations = std::sync::Arc::clone(&operations);
+        let random_starts = std::sync::Arc::clone(&random_starts);
+        let players = opts.players;
+        let tables = opts.tables;
+        std::thread::spawn(move || {
+            let mut schedule_generator =
+                schedule::Generator::new(rand::thread_rng(), players, tables);
+
+            tx.send(schedule_generator.best.clone()).unwrap();
+            loop {
+                let old_score = schedule_generator.best.get_score();
+                let mut local_operations: u64 = 0;
+                let mut local_random_starts: u64 = 0;
+                for _ in 0..PROCESS_LOOP_COUNT {
+                    let (ops, rs) = schedule_generator.process();
+                    local_operations += ops as u64;
+                    local_random_starts += rs as u64;
+                }
+
+                operations.fetch_add(local_operations, std::sync::atomic::Ordering::SeqCst);
+                random_starts.fetch_add(local_random_starts, std::sync::atomic::Ordering::SeqCst);
+
+                if schedule_generator.best.get_score() > old_score {
+                    tx.send(schedule_generator.best.clone()).unwrap();
+                }
+            }
+        });
+    }
+    let best_schedule = std::sync::Arc::new(std::sync::Mutex::new(rx.recv().unwrap()));
     {
-        let opts: Opts = Opts::parse();
-        let (tx, rx) = std::sync::mpsc::channel::<schedule::Schedule>();
-        let operations = std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0));
-        let random_starts = std::sync::Arc::new(std::sync::atomic::AtomicU64::new(0));
-        println!("Spawning {} threads", num_cpus::get());
-        for i in 0..num_cpus::get() {
-            let tx = tx.clone();
-            let operations = std::sync::Arc::clone(&operations);
-            let random_starts = std::sync::Arc::clone(&random_starts);
-            let players = opts.players;
-            let tables = opts.tables;
-            std::thread::spawn(move || {
-                let mut schedule_generator =
-                    schedule::Generator::new(rand::thread_rng(), players, tables);
-
-                tx.send(schedule_generator.best.clone()).unwrap();
-                loop {
-                    let old_score = schedule_generator.best.get_score();
-                    let mut local_operations: u64 = 0;
-                    let mut local_random_starts: u64 = 0;
-                    for _ in 0..PROCESS_LOOP_COUNT {
-                        let (ops, rs) = schedule_generator.process();
-                        local_operations += ops as u64;
-                        local_random_starts += rs as u64;
-                    }
-
-                    operations.fetch_add(local_operations, std::sync::atomic::Ordering::SeqCst);
-                    random_starts
-                        .fetch_add(local_random_starts, std::sync::atomic::Ordering::SeqCst);
-
-                    if schedule_generator.best.get_score() > old_score {
-                        tx.send(schedule_generator.best.clone()).unwrap();
-                    }
-                }
-            });
-        }
-        let best_schedule = std::sync::Arc::new(std::sync::Mutex::new(rx.recv().unwrap()));
-        {
-            let best_schedule = std::sync::Arc::clone(&best_schedule);
-            std::thread::spawn(move || loop {
-                let mut best_score = best_schedule.lock().unwrap().get_score();
-                let schedule = rx.recv().unwrap();
-                if schedule.get_score() > best_score {
-                    *best_schedule.lock().unwrap() = schedule;
-                    best_score = best_schedule.lock().unwrap().get_score();
-                }
-            });
-        }
-        let instant = std::time::Instant::now();
-        loop {
-            display_schedule(
-                &*best_schedule.lock().unwrap(),
-                operations.load(std::sync::atomic::Ordering::Relaxed),
-                random_starts.load(std::sync::atomic::Ordering::Relaxed),
-                instant.elapsed().as_nanos(),
-            );
-            std::thread::sleep(std::time::Duration::from_millis(1000));
-        }
+        let best_schedule = std::sync::Arc::clone(&best_schedule);
+        std::thread::spawn(move || loop {
+            let mut best_score = best_schedule.lock().unwrap().get_score();
+            let schedule = rx.recv().unwrap();
+            if schedule.get_score() > best_score {
+                *best_schedule.lock().unwrap() = schedule;
+                best_score = best_schedule.lock().unwrap().get_score();
+            }
+        });
+    }
+    let instant = std::time::Instant::now();
+    loop {
+        display_schedule(
+            &*best_schedule.lock().unwrap(),
+            operations.load(std::sync::atomic::Ordering::Relaxed),
+            random_starts.load(std::sync::atomic::Ordering::Relaxed),
+            instant.elapsed().as_nanos(),
+        );
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
+
+#[cfg(not(feature = "cli"))]
+fn main() {}
